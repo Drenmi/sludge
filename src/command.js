0d7740987f01c874b40eee014710fa6cc904d59f
Ted Johansson <drenmi@gmail.com>
2017-11-03 16:44:17 +0100

    Standardise usa of LoDash

--- src/command.js
+++ src/command.js
@@ -1,10 +1,12 @@
+const _ = require("lodash")
+
 const DEFAULT_SUCCESS_CALLBACK = function() { return "Yay!" }
 const DEFAULT_FAILURE_CALLBACK = function(message) { return message }
 
 function create({ guards = [], onSuccess = DEFAULT_SUCCESS_CALLBACK, onFailure = DEFAULT_FAILURE_CALLBACK }) {
   return function(actor, parameters) {
-    const results = guards.map(guard => guard(actor, parameters))
-    const failure = results.find(pass => pass !== true)
+    const results = _.map(guards, (guard) => (guard(actor, parameters)))
+    const failure = _.find(results, (pass) => (pass !== true))
 
     if(failure) {
       return onFailure(failure)
